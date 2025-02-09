3dceeaf98ea7601589936264f9f8afb0ba64eaf1
Francois Dickey <francois@dickey.com>
2025-02-08 19:18:17 -0500

    Using the Single page app application type, else PKCE won't work.

--- auth0/application.tf
+++ auth0/application.tf
@@ -2,7 +2,7 @@
 resource "auth0_client" "k3s_oidc" {
   name                = "K3S"
   description         = "My Cool App Client Created Through Terraform"
-  app_type            = "regular_web"
+  app_type            = "spa"
   callbacks           = ["http://localhost:8000"]
   cross_origin_auth   = true
   oidc_conformant     = true
